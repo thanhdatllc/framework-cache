ddef48d803095a96b66953ba8e10effffa3ea6ba
Thanh Dat LLC <thanhdatllc@outlook.com>
2016-06-24 01:25:32 +0700

    add func get-cache

---
+++ Thanh Dat LLC Frameworks.Cache/CacheTools.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thanh_Dat_LLC_Frameworks.Cache
+{
+    public static class CacheTools
+    {
+        public static string GetCache(string fileName)
+        {
+            var fullPath = AppDomain.CurrentDomain.BaseDirectory + @"Caches\" + fileName;
+            if (File.Exists(fullPath))
+            {
+                // Use StreamReader to consume the entire text file.
+                using (StreamReader reader = new StreamReader(fullPath))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            return null;
+        }
+        public static string GetCache(string path, string fileName)
+        {
+            var fullPath = path + fileName;
+            if (File.Exists(fullPath))
+            {
+                // Use StreamReader to consume the entire text file.
+                using (StreamReader reader = new StreamReader(fullPath))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            return null;
+        }
+    }
+}
